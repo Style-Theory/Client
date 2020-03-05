4af11566af4b0d9f9899526797a300bbfe48d9f7
mcc-6 <mcc-6@mcc-6s-Mac-mini.local>
2020-03-05 21:32:15 +0700

    Revisi Navbar

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 $("document").ready(() => {
-    $("#login-page").hide()
-    $("#login-btn-signup").on('click', (e) => {
-        e.preventDefault()
-        $("#login-page").slideDown()
-        $("#landing-page").css('filter', 'blur(3px)')
-    })
-    $("#xlogin").on('click', (e) => {  
-        $("#landing-page").css('filter', '')
-        $("#login-page").slideUp()
-    })
+    const token = localStorage.token
+    if(token) {
+        $("#login-page").hide()
+        $('.navbar').hide()
+        $("#login-btn-signup").on('click', (e) => {
+            e.preventDefault()
+            $("#login-page").slideDown()
+            $("#landing-page").css('filter', 'blur(3px)')
+        })
+        $("#xlogin").on('click', (e) => {  
+            $("#landing-page").css('filter', '')
+            $("#login-page").slideUp()
+        })
+    } else {
+        $("#login-page").hide()
+        $('.navbar').show()
+        $('#landing-page').hide()
+    }
 })
