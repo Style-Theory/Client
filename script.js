01c8c766972eaf4efd42fc068571b71f871ca309
MCC-02 <mcc-02@mcc-02s-Mac-mini.local>
2020-03-06 15:07:19 +0700

    crud ui

--- script.js
+++ script.js
@@ -275,29 +275,52 @@ function fetchDress(){
     .done(results => {
         $("#content-body-rent").append('<h2 id="rent-title"> View All</h2>')
         for(let i = 0; i < results.data.length/4; i++){
-                $("#content-body-rent").append(`<div class="container container-clothe"><div id=row-dress-${i} class="row"></div></div>`)
-                for(let j = i*4; j < (i+1)*4; j++){
-                    if(j >= results.data.length) {
-                        $(`#row-dress-${i}`).append(`<div class="col-sm">
-                            <div class="card" style="width: 12vw;">
-                                </div>
+            $("#content-body-rent").append(`<div class="container container-clothe"><div id=row-dress-${i} class="row"></div></div>`)
+            for(let j = i*4; j < (i+1)*4; j++){
+                if(j >= results.data.length) {
+                    $(`#row-dress-${i}`).append(`<div class="col-sm">
+                        <div class="card" style="width: 12vw;">
                             </div>
-                        </div>`)
-                    }else{
-                        $(`#row-dress-${i}`).append(`<div class="col-sm">
-                            <div class="card" style="width: 12vw;">
-                                <img src="./img/dress-sample.jpg" class="card-img-top clothe-card" alt="clothe">
-                                <div class="card-body">
-                                    <p class="card-text clothe-renter-card">${results.data[j].UserId} Adam Sementara</p>
-                                    <p class="card-text clothe-name-card">${results.data[j].name}</p>
-                                    <p class="card-text clothe-price-card">Rp. ${parsePrice(results.data[j].price)}</p>
-                                </div>
+                        </div>
+                    </div>`)
+                }else{
+                    $(`#row-dress-${i}`).append(`<div class="col-sm">
+                        <div class="card" style="width: 12vw;" id=card-${results.data[j].id}>
+                            <img src="./img/dress-sample.jpg" class="card-img-top clothe-card" alt="clothe">
+                            <div class="card-body">
+                                <p class="card-text clothe-renter-card">${results.data[j].UserId} Adam Sementara</p>
+                                <p class="card-text clothe-name-card">${results.data[j].name}</p>
+                                <p class="card-text clothe-price-card">Rp. ${parsePrice(results.data[j].price)}</p>
                             </div>
-                        </div>`)
-                    }
+                        </div>
+                    </div>`)
+                    $(`#card-${results.data[j].id}`).on('click', () => {
+                        console.log('wlwl')
+                        let today = new Date()
+                        $.ajax({
+                            url:`http://localhost:3000/rent/${results.data[j].id}`,
+                            method: 'PUT',
+                            headers: {
+                                token: localStorage.getItem('token')
+                            },
+                            data: {
+                                due_date: new Date(today.getTime() + 7*24*60*60*1000)
+                            }
+                        })
+                        .done(result => {
+                            resetDress()
+                        })
+                    })
                 }
             }
-        })
+        } 
+    })
+    
+}
+
+function resetDress() {
+    $("#content-body-rent").empty()
+    fetchDress()
 }
 
 function fetchMyStuff(){
@@ -310,6 +333,16 @@ function fetchMyStuff(){
     })
     .done(results => {
         $("#my-stuff").append('<h1 id="my-stuff-title">My Stuff</h1>')
+        $("#my-stuff").append('<button type="button" id="create-btn" class="btn btn-light" style="margin-left: 1rem;">Add Stuff</button>')
+        $("#create-btn").on('click', (e) => {
+            e.preventDefault()
+            $("#form-create-page").slideDown()
+            $("#my-stuff").css('filter', 'blur(3px)')
+        })
+        $("#xcreate").on('click', (e) => {  
+            $("#my-stuff").css('filter', '')
+            $("#form-create-page").slideUp()
+        })
                 for(let i = 0; i < results.data.length/3; i++){
                 $("#my-stuff").append(`<div class="container" style="margin: 0;"><div class="row" style="width:80vw" id="row-my-stuff-${i}"></div></div>`)
                     for(let j = i*3; j < (i+1)*3; j++){
@@ -322,7 +355,7 @@ function fetchMyStuff(){
                         }
                         else {
                             $(`#row-my-stuff-${i}`).append(`<div class="col-sm">
-                            <div class="card mb-3" style="max-width: 480px;">
+                            <div class="card mb-3" style="max-width: 480px;" >
                                 <div class="row no-gutters">
                                 <div class="col-md-2 displayflex">
                                     <img src="/img/list-dress-MyStuff.jpg" class="card-img card-img-my-stuff" alt="asd">
@@ -334,18 +367,112 @@ function fetchMyStuff(){
                                             <p class="card-text">Rp. ${parsePrice(results.data[j].price)}</p>
                                         </div>
                                         <div class="my-stuff-config">
-                                            <button type="button" class="btn btn-light btn-my-stuff">Edit</button>
-                                            <button type="button" class="btn btn-light btn-my-stuff">Delete</button>
+                                            <button type="button" class="btn btn-light btn-my-stuff" id=edit-mystuff-${results.data[j].id}>Edit</button>
+                                            <button type="button" class="btn btn-light btn-my-stuff" id=delete-mystuff-${results.data[j].id}>Delete</button>
                                         </div>
                                     </div>
                                 </div>
                                 </div>
                             </div>
                         </div>`)
+                        $(`#delete-mystuff-${results.data[j].id}`).on('click', () => {
+                            deleteMyStuff(results.data[j].id)
+                        })
+                        $(`#edit-mystuff-${results.data[j].id}`).on('click', () => {
+                            editMyStuff(results.data[j].id)
+                        })
+                        $("body").append(`<section id="form-edit-page-${results.data[j].id}" class="form-edit-page">
+                        <div class="edit-form-div">
+                            <p id="xedit-${results.data[j].id}" class="x">x</p>
+                            <form id="form-edit-${results.data[j].id}" style="margin: 0 50px 40px 40px;">
+                                <h1 class="edit-title">Edit your item</h1>
+                                <div class="form-group">
+                                        <label for="name">Name :</label>
+                                        <input type="text" class="form-control" id="edit-name-${results.data[j].id}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="price">Price :</label>
+                                    <input type="text" class="form-control" id="edit-price-${results.data[j].id}">
+                                </div>
+                                <button type="submit" class="btn btn-warning" style="background-color: silver; border-width: 0; cursor: pointer; margin-left: 0.5rem;">Edit</button>
+                            </form>
+                        </div>    
+                        </section>`)
+                        $(`#form-edit-page-${results.data[j].id}`).hide()
+                        $(`#edit-mystuff-${results.data[j].id}`).on('click', (e) => {
+                            e.preventDefault()
+                            $(`#form-edit-page-${results.data[j].id}`).slideDown()
+                            $("#my-stuff").css('filter', 'blur(3px)')
+                        })
+                        $(`#xedit-${results.data[j].id}`).on('click', (e) => {  
+                            $("#my-stuff").css('filter', '')
+                            $(`#form-edit-page-${results.data[j].id}`).slideUp()
+                        })
                     }
                 }
             }
         })
+}
+
+function editMyStuff(id){
+    $.ajax({
+        url: `http://localhost:3000/${id}`,
+        method: 'GET',
+        headers : {
+            token: localStorage.getItem('token')
+        }
+    })
+    .done(result => {
+        $(`#edit-name-${id}`).val(result.data.name)
+        $(`#edit-price-${id}`).val(result.data.price)
+        $(`#form-edit-page-${id}`).on('submit', (e) => {
+            e.preventDefault()
+            const data = {
+                name: $(`#edit-name-${id}`).val(),
+                price: $(`#edit-price-${id}`).val()
+            }
+            $.ajax({
+                url:`http://localhost:3000/${id}`,
+                method: 'PUT',
+                headers : {
+                    token: localStorage.getItem('token')
+                },
+                data
+            })
+            .done(result => {
+                fetchMyStuff()
+                $("#my-stuff").empty()
+                $("#my-stuff").css('filter', '')
+                $(`#form-edit-page-${id}`).slideUp()
+            })
+        })
+    })
+}
+
+function deleteMyStuff(id){
+    $.ajax({
+        url: `http://localhost:3000/${id}`,
+        method: 'DELETE',
+        headers : {
+            token: localStorage.getItem('token')
+        }
+    })
+    .done(result => {
+        $("#my-stuff").empty()
+        $("#my-stuff").css('filter', '')
+        fetchMyStuff()
+        $("#dashboard-home-page").hide()
+        $("#form-create-page").hide()
+        $('#rent').hide()
+        $('#my-order').hide()
+        $("#my-order").empty()
+        $('#my-stuff').show()
+        $('#rent').hide()
+        $("#login-page").hide()
+        $('.navbar1').show()
+        $("#content-body-rent").empty()
+        $('#landing-page').hide()
+    })
 }
 
 function fetchMyOrder(){
@@ -379,14 +506,14 @@ function fetchMyOrder(){
                                       </div>
                                       <div class="col-md-5">
                                         <div class="card-body">
-                                            <h5 class="card-title">Dress Name</h5>
+                                            <h5 class="card-title">${results.data[j].name}</h5>
                                             <div style="display: flex;">
                                                 <p class="card-text" style="width: 50%;">harga</p>
                                                 <p class="card-text" style="width: 50%;">return</p>
                                             </div>
                                             <div style="display: flex;">
-                                                <p class="card-text" style="width: 50%;">100.000</p>
-                                                <p class="card-text" style="width: 50%;">30 Maret</p>
+                                                <p class="card-text" style="width: 50%;">Rp. ${parsePrice(results.data[j].price)}</p>
+                                                <p class="card-text" style="width: 50%;">${results.data[j].due_date}</p>
                                             </div>
                                             <button type="button" class="btn btn-dark">Return</button>
                                         </div>
@@ -454,50 +581,3 @@ function resetLandingPage (){
     $("#login-password").val('')
     $("#landing-page").css('filter', '')    
 }
-
-// ! ini untuk fetch location
-let areaJakarta = {
-	"async": true,
-	"crossDomain": true,
-	"url": "http://dev.farizdotid.com/api/daerahindonesia/provinsi/31/kabupaten",
-	"method": "GET",
-}
-let areaJakarta = {
-	"async": true,
-	"crossDomain": true,
-	"url": "http://dev.farizdotid.com/api/daerahindonesia/provinsi/31/kabupaten/3101/kecamatan",
-	"method": "GET",
-}
-let areaJakarta = {
-	"async": true,
-	"crossDomain": true,
-	"url": "http://dev.farizdotid.com/api/daerahindonesia/provinsi/31/kabupaten/3171/kecamatan",
-	"method": "GET",
-}
-let areaJakarta = {
-	"async": true,
-	"crossDomain": true,
-	"url": "http://dev.farizdotid.com/api/daerahindonesia/provinsi/31/kabupaten/3172/kecamatan",
-	"method": "GET",
-}
-let areaJakarta = {
-	"async": true,
-	"crossDomain": true,
-	"url": "http://dev.farizdotid.com/api/daerahindonesia/provinsi/31/kabupaten/3173/kecamatan",
-	"method": "GET",
-}
-let areaJakarta = {
-	"async": true,
-	"crossDomain": true,
-	"url": "http://dev.farizdotid.com/api/daerahindonesia/provinsi/31/kabupaten/3174/kecamatan",
-	"method": "GET",
-}
-let areaJakarta = {
-	"async": true,
-	"crossDomain": true,
-	"url": "http://dev.farizdotid.com/api/daerahindonesia/provinsi/31/kabupaten/3175/kecamatan",
-	"method": "GET",
-}
-$.ajax(areaJakarta).done(function (response) {
-	console.log(response);
-});
